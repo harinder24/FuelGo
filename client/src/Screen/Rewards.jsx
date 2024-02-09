b8ca68c5c75bcb4b3a30a45563cdc79b210c3bc1
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-02-08 23:23:15 -0800

    reward page done

--- client/src/Screen/Rewards.jsx
+++ client/src/Screen/Rewards.jsx
@@ -3,6 +3,7 @@ import Profile, { ProfilePopUp } from '../Components/User/Profile';
 import TopNav from '../Components/User/TopNav';
 import { SiStarbucks, SiAmazon } from 'react-icons/si';
 import { TbLetterC } from 'react-icons/tb';
+import { MdClose } from 'react-icons/md';
 import { FaTag } from 'react-icons/fa6';
 
 const rewardsList = [
@@ -23,7 +24,7 @@ const rewardsList = [
     icon: <FaTag className='text-[#FFD147]' />,
   },
 ];
-const rewardItems = [
+const amountOptions = [
   { amount: 25, point: 500 },
   { amount: 50, point: 1000 },
   { amount: 100, point: 2000 },
@@ -34,10 +35,8 @@ export default function Rewards() {
   const [isProfilePopUp, setIsProfilePopUp] = useState(false);
   const [isList, setIsList] = useState(true);
   const [point, setPoint] = useState(3253);
-
-  const handleClick = (price) => {
-    setPoint((prev) => prev - price);
-  };
+  const [isPurchasing, setIsPurchasing] = useState(false);
+  const [reward, setReward] = useState();
 
   async function setIsProfilePopUpHandler() {
     setIsProfilePopUp(true);
@@ -45,14 +44,26 @@ export default function Rewards() {
 
   return (
     <>
-      <div className='w-screen h-screen  bg-lightMode-bg dark:bg-darkMode-bg '>
+      {isPurchasing && (
+        <PurchaseModal
+          point={point}
+          setPoint={setPoint}
+          setIsPurchasing={setIsPurchasing}
+          reward={reward}
+        />
+      )}
+      <div
+        className={`w-screen h-screen overflow-hidden  bg-lightMode-bg dark:bg-darkMode-bg ${
+          isPurchasing && 'blur-sm brightness-50 pointer-events-none'
+        }`}
+      >
         <div className=' absolute w-screen h-screen z-20'>
           <div className=' flex w-full h-full justify-center'>
             <div className='   w-[1400px] min-w-auto  h-full flex flex-row gap-x-3 p-3 max-[520px]:p-0 overflow-hidden'>
               <div className='w-[312px] h-full rounded-xl max-[1000px]:rounded-none max-[1230px]:hidden'>
                 <Profile />
               </div>
-              <div className='flex-1 flex flex-col '>
+              <div className='flex-1 flex flex-col overflow-auto'>
                 <TopNav
                   setIsProfilePopUpHandler={setIsProfilePopUpHandler}
                   isList={isList}
@@ -66,39 +77,11 @@ export default function Rewards() {
                     {point.toLocaleString('en-US')} P
                   </h4>
                 </div>
-                <div className='flex flex-wrap w-full justify-center gap-x-20 mt-24 overflow-scroll '>
-                  {rewardsList.map((reward) => (
-                    <div
-                      key={reward.title}
-                      className='flex flex-col items-center'
-                    >
-                      <div
-                        className={`w-fit  p-3 mb-4 rounded-full sbg text-2xl`}
-                      >
-                        {reward.icon}
-                      </div>
-                      <span className='text-white mb-7'>{reward.title}</span>
-                      {rewardItems.map((item) => (
-                        <button
-                          onClick={() => handleClick(item.point)}
-                          key={`amount-${item.amount}`}
-                          className='flex justify-between w-40 mb-2 px-4 py-2 rounded-lg border-t-[1px] border-lightMode-fbg tbg-gradient'
-                        >
-                          <span className='text-white'>$ {item.amount}</span>
-                          <span
-                            className={
-                              item.point > point
-                                ? 'text-darkMode-error'
-                                : 'text-darkMode-valid'
-                            }
-                          >
-                            {item.point} P
-                          </span>
-                        </button>
-                      ))}
-                    </div>
-                  ))}
-                </div>
+                <RewardsList
+                  point={point}
+                  setIsPurchasing={setIsPurchasing}
+                  setReward={setReward}
+                />
               </div>
             </div>
           </div>
@@ -108,3 +91,111 @@ export default function Rewards() {
     </>
   );
 }
+
+function RewardsList({ point, setIsPurchasing, setReward }) {
+  const handleClick = (reward, item) => {
+    setIsPurchasing(true);
+
+    const { title, icon } = reward;
+    const { amount, point: requiredPoint } = item;
+
+    setReward({ title, icon, amount, requiredPoint });
+  };
+
+  return (
+    <div className='flex flex-wrap w-full justify-center gap-x-20 mt-24 '>
+      {rewardsList.map((reward) => (
+        <div key={reward.title} className='flex flex-col items-center mb-16'>
+          <div className={`w-fit  p-3 mb-4 rounded-full sbg text-2xl `}>
+            {reward.icon}
+          </div>
+          <span className='text-white mb-7'>{reward.title}</span>
+          {amountOptions.map((item) => (
+            <button
+              onClick={() => handleClick(reward, item)}
+              key={`amount-${item.amount}`}
+              className='flex justify-between w-40 mb-2 px-4 py-2 rounded-lg border-t-[1px] border-lightMode-fbg tbg-gradient'
+              disabled={item.point > point}
+            >
+              <span className='text-white'>$ {item.amount}</span>
+              <span
+                className={
+                  item.point > point
+                    ? 'text-darkMode-error'
+                    : 'text-darkMode-valid'
+                }
+              >
+                {item.point} P
+              </span>
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function PurchaseModal({ point, setPoint, setIsPurchasing, reward }) {
+  const [showCode, setShowCode] = useState(false);
+
+  const { title, icon, amount, requiredPoint } = reward;
+
+  const hardCodedCoupon = '4XC2-23BE-3TO7-2Y6P';
+  const balance = point - requiredPoint;
+
+  const handleClick = () => {
+    setIsPurchasing(false);
+    setShowCode(false);
+  };
+  const getCoupon = () => {
+    setPoint(balance);
+    setShowCode(true);
+  };
+
+  return (
+    <div
+      className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5/6 max-w-96 ${
+        showCode ? 'max-h-60' : 'max-h-96'
+      } h-5/6 border-t-[1px] border-lightMode-fbg bg-lightMode-sbg dark:bg-darkMode-sbg rounded-xl p-4 z-10 overflow-auto`}
+    >
+      <div className='flex justify-end w-full text-darkMode-border min-w-36'>
+        <button onClick={handleClick}>
+          <MdClose />
+        </button>
+      </div>
+      <div className='flex flex-col items-center w-full mt-6 mb-6 min-w-36'>
+        <div className='text-4xl mb-2 min'>{icon}</div>
+        <h2 className='text-xl text-white font-medium mb-1'>{title}</h2>
+        <span className=' text-sm text-darkMode-valid'>$ {amount}</span>
+      </div>
+      {showCode ? (
+        <div className='flex flex-col w-full gap-5 items-center min-w-36'>
+          <div className='w-full border-[0.5px] border-solid border-darkMode-border rounded py-2 pl-2 text-darkMode-header text-xs '>
+            {hardCodedCoupon}
+          </div>
+        </div>
+      ) : (
+        <div className='flex flex-col w-full gap-5 items-center min-w-36'>
+          <div className='w-full border-[0.5px] border-solid border-darkMode-border rounded text-right py-2 pr-2 text-darkMode-valid text-xs '>
+            {point} Points
+          </div>
+          <div className='flex justify-between w-full border-[0.5px] border-solid border-darkMode-border rounded text-right py-2 px-2 text-darkMode-error text-xs '>
+            <span className='font-black tracking-[-0.2rem]'>--</span>
+            <span>{requiredPoint} Points</span>
+          </div>
+          <div className='w-full border-[0.5px] border-solid border-darkMode-border rounded text-right py-2 pr-2 text-darkMode-valid text-xs '>
+            {balance} Points
+          </div>
+          <button
+            onClick={getCoupon}
+            className='w-fit py-1.5 px-4 font-medium rounded-lg bg-darkMode-valid text-sm'
+          >
+            Get
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+//border-t-[1px] border-lightMode-fbg tbg-gradient
+// bg-lightMode-sbg dark:bg-darkMode-sbg
