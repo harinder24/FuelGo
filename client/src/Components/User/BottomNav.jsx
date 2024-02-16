06d16c7461dd5d9ebfb3d2efd529d6bf15d1c393
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-02-16 11:35:01 -0800

    merge issue fixed

--- client/src/Components/User/BottomNav.jsx
+++ client/src/Components/User/BottomNav.jsx
@@ -1,62 +1,62 @@
-import React, { useEffect } from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import HomeIcon from "@mui/icons-material/Home";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import WalletOutlinedIcon from "@mui/icons-material/WalletOutlined";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import WalletOutlinedIcon from '@mui/icons-material/WalletOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useLocation, useNavigate } from 'react-router-dom';
 export default function BottomNav() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   const pathName = useLocation().pathname;
- 
+
   return (
-    <div className="h-[56px] w-full flex flex-row justify-evenly items-center sbg border-t-[1px] cborder">
+    <div className='h-[56px] w-full flex flex-row justify-evenly items-center sbg border-t-[1px] cborder'>
       <div
-      onClick={()=> navigate("/home")}
+        onClick={() => navigate('/home')}
         className={` ${
-          pathName === "/home"
-            ? "tb"
-            : "tp hover:text-lightMode-header dark:hover:text-darkMode-header"
+          pathName === '/home'
+            ? 'tb'
+            : 'tp hover:text-lightMode-header dark:hover:text-darkMode-header'
         }  cursor-pointer `}
       >
         <HomeIcon sx={{ fontSize: 30 }} />
       </div>
       <div
-       onClick={()=> navigate("/search")}
+        onClick={() => navigate('/search')}
         className={` ${
-          pathName === "/search"
-            ? "tb"
-            : "tp hover:text-lightMode-header dark:hover:text-darkMode-header"
+          pathName === '/search'
+            ? 'tb'
+            : 'tp hover:text-lightMode-header dark:hover:text-darkMode-header'
         }  cursor-pointer `}
       >
         <SearchIcon sx={{ fontSize: 30 }} />
       </div>
       <div
-       onClick={()=> navigate("/rewards")}
+        onClick={() => navigate('/rewards')}
         className={` ${
-          pathName === "/rewards"
-            ? "tb"
-            : "tp hover:text-lightMode-header dark:hover:text-darkMode-header"
+          pathName === '/rewards'
+            ? 'tb'
+            : 'tp hover:text-lightMode-header dark:hover:text-darkMode-header'
         }  cursor-pointer `}
       >
         <WalletOutlinedIcon sx={{ fontSize: 30 }} />
       </div>
       <div
-       onClick={()=> navigate("/favourite")}
+        onClick={() => navigate('/favourite')}
         className={` ${
-          pathName === "/favourite"
-            ? "tb"
-            : "tp hover:text-lightMode-header dark:hover:text-darkMode-header"
+          pathName === '/favourite'
+            ? 'tb'
+            : 'tp hover:text-lightMode-header dark:hover:text-darkMode-header'
         }  cursor-pointer `}
       >
         <FavoriteIcon sx={{ fontSize: 30 }} />
       </div>
       <div
-       onClick={()=> navigate("/profile")}
+        onClick={() => navigate('/profile')}
         className={` ${
-          pathName === "/profile"
-            ? "tb"
-            : "tp hover:text-lightMode-header dark:hover:text-darkMode-header"
+          pathName === '/profile'
+            ? 'tb'
+            : 'tp hover:text-lightMode-header dark:hover:text-darkMode-header'
         }  cursor-pointer `}
       >
         <AccountCircleIcon sx={{ fontSize: 30 }} />
