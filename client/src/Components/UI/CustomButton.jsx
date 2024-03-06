ba2ad454f23acceec62e67b19f83d64ed1b8e5fb
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-03-05 16:10:28 -0800

    Reward Page Cleaned up

--- client/src/Components/UI/CustomButton.jsx
+++ client/src/Components/UI/CustomButton.jsx
@@ -9,21 +9,26 @@ export default function CustomButton({
   handleClick,
   rounded,
   hoverDarker,
+  disable,
 }) {
   const fontSizeCSS = 'text-' + (fontSize || 'sm');
   const roundedCSS = 'rounded-' + (rounded || 'lg');
+  const hoverCSS = disable
+    ? ''
+    : hoverDarker
+    ? 'hover:brightness-90'
+    : 'hover:brightness-125';
 
   return (
     <button
-      className={`px-2 h-10 ${fontSizeCSS}  th ${
-        hoverDarker ? 'hover:brightness-90' : 'hover:brightness-125'
-      } ${roundedCSS}`}
+      className={`px-2 h-10 ${fontSizeCSS}  th ${hoverCSS} ${roundedCSS}`}
       style={{
         backgroundColor: bgColor ? bgColor : '#0BA5E9',
         width: width ? width : '100%',
         height: height ? height : '2.5ren',
       }}
       onClick={handleClick}
+      disabled={disable}
     >
       {children || 'Submit'}
     </button>
