466702aa2901013cd915c03dcd67b44a1f194bd5
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-03-08 17:48:00 -0800

    Account related pages cleaned up

--- client/src/Screen/Signup.jsx
+++ client/src/Screen/Signup.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
-import AuthPageUiWrapper from '../Components/Auth/AuthPageUiWrapper'
+import React, { useState } from 'react';
+import CustomInput from '../Components/UI/CustomInput';
+import { MdLockOutline, MdOutlineMail } from 'react-icons/md';
+import CustomButton from '../Components/UI/CustomButton';
+import { useNavigate } from 'react-router-dom';
+import GoogleLoginBtn from '../Components/Auth/GoogleLoginBtn';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [isAgreedToTerms, setIsAgreedToTerms] = useState(false);
+  // return (
+  //   <AuthPageUiWrapper isLogin={false}/>
+  // );
+  const handleSubmit = () => {};
   return (
-    <AuthPageUiWrapper isLogin={false}/>
-  )
+    <div className='h-full  '>
+      <h2 className=' text-white text-2xl font-semibold mb-4'>
+        Create an account
+      </h2>
+      <form onSubmit={handleSubmit} noValidate>
+        <div className='flex flex-col gap-y-6 mb-6 mt-6'>
+          <CustomInput
+            label='Email *'
+            paddingLeft='40px'
+            placeHolder='Email'
+            handleChange={(e) => setEmail(e.target.value)}
+          >
+            <MdOutlineMail className='absolute left-2 tp text-2xl bottom-[8px]' />
+          </CustomInput>
+          <CustomInput
+            label='Password *'
+            paddingLeft='40px'
+            placeHolder='Password'
+            handleChange={(e) => setPassword(e.target.value)}
+          >
+            <MdLockOutline className='absolute left-2 tp text-2xl bottom-[8px]' />
+          </CustomInput>
+        </div>
+        <label className='inline-flex items-center mb-3'>
+          <input
+            onClick={() => setIsAgreedToTerms((prevState) => !prevState)}
+            type='checkbox'
+            className='form-checkbox h-[14px] w-[14px]  transition duration-150 ease-in-out checkbox border-[1px] border-lightMode-border dark:border-darkMode-border:'
+            checked={isAgreedToTerms}
+          />
+          <span className='ml-2 text-sm text-lightMode-p dark:text-darkMode-p'>
+            I agree to the terms and conditions
+          </span>
+        </label>
+        {/* ToDo: add real function on login button and signin button*/}
+        <CustomButton />
+      </form>
+      <div className=' text-lightMode-p text-sm dark:text-darkMode-p mt-4 mb-4 flex flex-row w-full items-center justify-center'>
+        <h4 className='flex flex-row text-sm'>
+          Already have an account?
+          <button
+            onClick={() => navigate('../login')}
+            className='pl-1  text-lightMode-button dark:text-lightMode-button cursor-pointer'
+          >
+            Login Now
+          </button>
+        </h4>
+      </div>
+
+      <div className='w-full relative mt-1'>
+        <div className='absolute top-0 border-b-[1px] border-lightMode-p dark:border-darkMode-p w-full'>
+          <div className='absolute top-[-10px] text-lightMode-p dark:text-darkMode-p w-full'>
+            <div className='w-full flex flex-row justify-center'>
+              <div
+                className='  text-sm bg-lightMode-sbg dark:bg-darkMode-sbg max-[640px]:bg-lightMode-bg
+                dark:max-[640px]:bg-darkMode-bg rounded-full
+                px-1'
+              >
+                or
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <GoogleOAuthProvider clientId='698921458629-k410ff2u0hnkl6bap113t8f9vepj8eoq.apps.googleusercontent.com'>
+        <GoogleLoginBtn />
+      </GoogleOAuthProvider>
+    </div>
+  );
 }
