ae99824b3b53daaada974668a34fd2ed1465d923
harinder24 <hss022002@gmail.com>
2024-03-12 12:16:16 -0700

    3 api added
    add, edit and delete comment

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,9 @@ import {
   sendUserData,
   getGasStations,
   getGasStationData,
+  addComment,
+  editComment,
+  deleteComment,
 } from "../controller/user.js";
 import validateToken from "../middleware/verifyToken.js";
 const userRouter = express.Router();
@@ -10,5 +13,8 @@ const userRouter = express.Router();
 userRouter.get("/getuserdata", validateToken, sendUserData);
 userRouter.post("/getgasstations", getGasStations);
 userRouter.get("/getgasstationdata/:id", validateToken, getGasStationData);
+userRouter.post("/addcomment", validateToken, addComment);
+userRouter.post("/editcomment", validateToken, editComment);
+userRouter.post("/addcomment", validateToken, deleteComment);
 
 export { userRouter };
